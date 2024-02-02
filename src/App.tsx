1d5789d79cbc2483c74da06f3fb0d631b0fea2a0
Elliot Harding <elliot@hardings.org.uk>
2024-02-02 19:16:08 +0000

    Install Material Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import {Box, Container, Typography} from "@mui/material";
 
 function App() {
 
   return (
     <>
-
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+            Material UI Create React App example in TypeScript
+          </Typography>
+        </Box>
+      </Container>
     </>
   )
 }
